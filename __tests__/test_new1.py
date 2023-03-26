3ac65fd7e83491307192b2f97b12a2ca50a0f847
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-03-26 19:54:08 +0530

    Update test_new1.py

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -1,78 +1,28 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 
 import serial
 import pyautogui
 
-class TestArduinoSerial(unittest.TestCase):
+class TestSerialCommunication(unittest.TestCase):
 
     @patch('serial.Serial')
-    def test_next_tab(self, mock_serial):
-        mock_data = b'next'
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.return_value = mock_data
-        mock_serial.return_value = mock_serial_instance
-
-        with patch('pyautogui.hotkey') as mock_hotkey:
-            arduino = ArduinoSerial('com5', 9600)
-            arduino.read_serial()
-
-            mock_hotkey.assert_called_with('ctrl', 'pgdn')
-    
-    @patch('serial.Serial')
-    def test_previous_tab(self, mock_serial):
-        mock_data = b'previous'
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.return_value = mock_data
-        mock_serial.return_value = mock_serial_instance
-
-        with patch('pyautogui.hotkey') as mock_hotkey:
-            arduino = ArduinoSerial('com5', 9600)
-            arduino.read_serial()
-
-            mock_hotkey.assert_called_with('ctrl', 'pgup')
-    
-    @patch('serial.Serial')
-    def test_scroll_down(self, mock_serial):
-        mock_data = b'down'
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.return_value = mock_data
-        mock_serial.return_value = mock_serial_instance
-
-        with patch('pyautogui.scroll') as mock_scroll:
-            arduino = ArduinoSerial('com5', 9600)
-            arduino.read_serial()
-
-            mock_scroll.assert_called_with(-10)
-    
-    @patch('serial.Serial')
-    def test_scroll_up(self, mock_serial):
-        mock_data = b'up'
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.return_value = mock_data
-        mock_serial.return_value = mock_serial_instance
-
-        with patch('pyautogui.keyDown') as mock_keyDown, patch('pyautogui.keyUp') as mock_keyUp:
-            arduino = ArduinoSerial('com5', 9600)
-            arduino.read_serial()
-
-            mock_keyDown.assert_called_with('pg up')
-            mock_keyUp.assert_called_with('pg up')
-    
-    @patch('serial.Serial')
-    def test_switch_tab(self, mock_serial):
-        mock_data = b'change'
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.return_value = mock_data
-        mock_serial.return_value = mock_serial_instance
-
-        with patch('pyautogui.keyDown') as mock_keyDown, patch('pyautogui.press') as mock_press, patch('pyautogui.keyUp') as mock_keyUp:
-            arduino = ArduinoSerial('com5', 9600)
-            arduino.read_serial()
-
-            mock_keyDown.assert_called_with('alt')
-            mock_press.assert_called_with('tab')
-            mock_keyUp.assert_called_with('alt')
-
-if __name__ == '__main__':
-    unittest.main()
+    @patch('pyautogui.hotkey')
+    @patch('pyautogui.scroll')
+    @patch('pyautogui.keyDown')
+    @patch('pyautogui.keyUp')
+    def test_new1(self, mock_keyUp, mock_keyDown, mock_scroll, mock_hotkey, mock_serial):
+        
+        # Set up mock serial object and input
+        mock_serial.return_value.readline.return_value = b'next\n'
+        
+        # Call the function to be tested
+        main()
+        
+        # Check that expected functions were called with expected inputs
+        mock_serial.assert_called_with('com5', 9600)
+        mock_serial.return_value.readline.assert_called()
+        mock_hotkey.assert_called_with('ctrl', 'pgdn')
+        mock_keyUp.assert_not_called()
+        mock_keyDown.assert_not_called()
+        mock_scroll.assert_not_called()
