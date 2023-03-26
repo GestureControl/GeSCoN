3b4cfc7eefdd8898f1b2b8be85f394e8cdf41eba
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-03-26 13:50:36 +0530

    Create test_new1.py
    I have created a unit test for python file to automate testing using pytest module

---
+++ __tests__/test_new1.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import patch, Mock
+import serial
+import pyautogui
+import io
+import sys
+
+class TestGestureControl(unittest.TestCase):
+
+    @patch('serial.Serial')
+    @patch('pyautogui.hotkey')
+    @patch('pyautogui.scroll')
+    @patch('pyautogui.keyDown')
+    @patch('pyautogui.keyUp')
+    def test_gesture_control(self, mock_serial, mock_hotkey, mock_scroll, mock_keydown, mock_keyup):
+        test_cases = [
+            ("next", pyautogui.hotkey('ctrl', 'pgdn')),
+            ("previous", pyautogui.hotkey('ctrl', 'pgup')),
+            ("down", pyautogui.scroll(-10)),
+            ("up", pyautogui.keyDown('pg up'), pyautogui.keyUp('pg up')),
+            ("change", pyautogui.keyDown('alt'), pyautogui.press('tab'), pyautogui.keyUp('alt'))
+        ]
+        serial_data = ["{}".format(test_case[0]).encode('utf-8') for test_case in test_cases]
+        mock_serial_instance = Mock(spec=serial.Serial)
+        mock_serial_instance.readline.side_effect = serial_data
+        mock_serial.return_value = mock_serial_instance
+
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            exec(open('gesture_control.py').read())
+            output = fake_out.getvalue()
+
+        for i in range(len(test_cases)):
+            self.assertIn(test_cases[i][0], output)
+            if len(test_cases[i]) > 1:
+                self.assertIn(test_cases[i][1], mock_hotkey.mock_calls)
+
+if __name__ == '__main__':
+    unittest.main()
