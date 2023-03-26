a6d19f288dd9a4770c941671a989668d0ab38985
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-03-26 14:23:17 +0530

    Update test_new1.py
    correcting indentation error occured in previous workflow action

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -5,38 +5,31 @@ import pyautogui
 import io
 import sys
 
-def test_gesture_control(mocker):
-    mock_serial_instance = Mock(spec=serial.Serial)
-    mocker.patch('serial.Serial', return_value=mock_serial_instance)
+class TestGestureControl:
+    
+    @patch('serial.Serial')
+    @patch('pyautogui.hotkey')
+    @patch('pyautogui.scroll')
+    @patch('pyautogui.keyDown')
+    @patch('pyautogui.keyUp')
+    def test_gesture_control(self, mock_serial, mock_hotkey, mock_scroll, mock_keydown, mock_keyup):
+        test_cases = [
+            ("next", pyautogui.hotkey('ctrl', 'pgdn')),
+            ("previous", pyautogui.hotkey('ctrl', 'pgup')),
+            ("down", pyautogui.scroll(-10)),
+            ("up", pyautogui.keyDown('pg up'), pyautogui.keyUp('pg up')),
+            ("change", pyautogui.keyDown('alt'), pyautogui.press('tab'), pyautogui.keyUp('alt'))
+        ]
+        serial_data = ["{}".format(test_case[0]).encode('utf-8') for test_case in test_cases]
+        mock_serial_instance = Mock(spec=serial.Serial)
+        mock_serial_instance.readline.side_effect = serial_data
+        mock_serial.return_value = mock_serial_instance
 
-    mock_hotkey = mocker.patch('pyautogui.hotkey')
-    mock_scroll = mocker.patch('pyautogui.scroll')
-    mock_keydown = mocker.patch('pyautogui.keyDown')
-    mock_keyup = mocker.patch('pyautogui.keyUp')
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            exec(open('gesture_control.py').read())
+            output = fake_out.getvalue()
 
-    test_cases = [
-        ("next", pyautogui.hotkey('ctrl', 'pgdn')),
-        ("previous", pyautogui.hotkey('ctrl', 'pgup')),
-        ("down", pyautogui.scroll(-10)),
-        ("up", pyautogui.keyDown('pg up'), pyautogui.keyUp('pg up')),
-        ("change", pyautogui.keyDown('alt'), pyautogui.press('tab'), pyautogui.keyUp('alt'))
-    ]
-    serial_data = ["{}".format(test_case[0]).encode('utf-8') for test_case in test_cases]
-    mock_serial_instance.readline.side_effect = serial_data
-
-    with patch('sys.stdout', new=io.StringIO()) as fake_out:
-        exec(open('gesture_control.py').read())
-        output = fake_out.getvalue()
-
-    for i in range(len(test_cases)):
-        assert test_cases[i][0] in output
-        if len(test_cases[i]) > 1:
-            mock_hotkey.assert_any_call(*test_cases[i][1])
-            mock_scroll.assert_any_call(*test_cases[i][1])
-            mock_keydown.assert_any_call(*test_cases[i][1])
-            mock_keyup.assert_any_call(*test_cases[i][1])
-
+        for i in range(len(test_cases)):
+            self.assertIn(test_cases[i][0], output)
+            if len(test_cases[i]) > 1:
                 self.assertIn(test_cases[i][1], mock_hotkey.mock_calls)
-
-if __name__ == '__main__':
-    unittest.main()
