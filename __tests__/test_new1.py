9e4b738d3385db52ca38bb5272cf2eafe3485ccc
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-04-05 17:37:38 +0530

    Update test_new1.py
    updating with unit test cases for new1.py file

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -1,37 +1,72 @@
-export DISPLAY=:192.168.182.248
-
-import pytest
-from unittest.mock import patch, Mock
-import serial
-import pyautogui
+import unittest
+from unittest.mock import MagicMock
 import io
 import sys
 
-class TestGestureControl:
-    
-    @patch('serial.Serial')
-    @patch('pyautogui.hotkey')
-    @patch('pyautogui.scroll')
-    @patch('pyautogui.keyDown')
-    @patch('pyautogui.keyUp')
-    def test_new1(self, mock_serial, mock_hotkey, mock_scroll, mock_keydown, mock_keyup):
-        test_cases = [
-            ("next", pyautogui.hotkey('ctrl', 'pgdn')),
-            ("previous", pyautogui.hotkey('ctrl', 'pgup')),
-            ("down", pyautogui.scroll(-10)),
-            ("up", pyautogui.keyDown('pg up'), pyautogui.keyUp('pg up')),
-            ("change", pyautogui.keyDown('alt'), pyautogui.press('tab'), pyautogui.keyUp('alt'))
+import my_code
+
+class TestMyCode(unittest.TestCase):
+
+    def setUp(self):
+        # create a mock serial object
+        self.mock_serial = MagicMock()
+        # set the return value of readline() method
+        self.mock_serial.readline.side_effect = [
+            b'next\r\n', b'up\r\n', b'previous\r\n', b'down\r\n', b'close\r\n'
         ]
-        serial_data = ["{}".format(test_case[0]).encode('utf-8') for test_case in test_cases]
-        mock_serial_instance = Mock(spec=serial.Serial)
-        mock_serial_instance.readline.side_effect = serial_data
-        mock_serial.return_value = mock_serial_instance
+        # set the mock serial object to be used by the code
+        my_code.Arduino_Serial = self.mock_serial
 
-        with patch('sys.stdout', new=io.StringIO()) as fake_out:
-            exec(open('gesture_control.py').read())
-            output = fake_out.getvalue()
+        # redirect stdout to a StringIO object
+        self.captured_output = io.StringIO()
+        sys.stdout = self.captured_output
 
-        for i in range(len(test_cases)):
-            self.assertIn(test_cases[i][0], output)
-            if len(test_cases[i]) > 1:
-                self.assertIn(test_cases[i][1], mock_hotkey.mock_calls)
+    def tearDown(self):
+        # reset stdout
+        sys.stdout = sys.__stdout__
+
+    def test_new1(self):
+        # test 'next' command
+        my_code.incoming_data = ""
+        my_code.Arduino_Serial.readline.return_value = b'next\r\n'
+        my_code.main()
+        self.assertEqual(
+            self.captured_output.getvalue().strip(),
+            "KeyboardEvent(down='ctrl', up='ctrl')\nKeyboardEvent(down='pgdn', up='pgdn')"
+        )
+
+        # test 'up' command
+        my_code.incoming_data = ""
+        my_code.Arduino_Serial.readline.return_value = b'up\r\n'
+        my_code.main()
+        self.assertEqual(
+            self.captured_output.getvalue().strip(),
+            "KeyboardEvent(down='pgup', up='pgup')"
+        )
+
+        # test 'previous' command
+        my_code.incoming_data = ""
+        my_code.Arduino_Serial.readline.return_value = b'previous\r\n'
+        my_code.main()
+        self.assertEqual(
+            self.captured_output.getvalue().strip(),
+            "KeyboardEvent(down='ctrl', up='ctrl')\nKeyboardEvent(down='pgup', up='pgup')"
+        )
+
+        # test 'down' command
+        my_code.incoming_data = ""
+        my_code.Arduino_Serial.readline.return_value = b'down\r\n'
+        my_code.main()
+        self.assertEqual(
+            self.captured_output.getvalue().strip(),
+            "KeyboardEvent(down='pgdn', up='pgdn')"
+        )
+
+        # test 'close' command
+        my_code.incoming_data = ""
+        my_code.Arduino_Serial.readline.return_value = b'close\r\n'
+        my_code.main()
+        self.assertEqual(
+            self.captured_output.getvalue().strip(),
+            "KeyboardEvent(down='alt', up='alt')\nKeyboardEvent(down='f4', up='f4')"
+        )
