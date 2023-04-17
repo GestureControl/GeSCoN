412f6359a84a6967a2ad6217a16f8bb00a883ba8
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-04-17 21:19:36 +0530

    Update test_new1.py

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -8,22 +8,22 @@ Arduino_Serial = serial.Serial('com12', 9600)
 def test_serial_communication():
     # Send 'next' command and check if it performs 'ctrl+pgdn' operation
     Arduino_Serial.write(b'next\n')
-    assert pyautogui.hotkey('ctrl', 'pgdn') == b'next\n'
+    assert pyautogui.hotkey('ctrl', 'pgdn') == 'next'
 
     # Send 'previous' command and check if it performs 'ctrl+pgup' operation
     Arduino_Serial.write(b'previous\n')
-    assert pyautogui.hotkey('ctrl', 'pgup') == b'previous\n'
+    assert pyautogui.hotkey('ctrl', 'pgup') == 'previous'
 
     # Send 'down' command and check if it scrolls down the page
     Arduino_Serial.write(b'down\n')
-    assert pyautogui.scroll(-100) == b'down\n'
+    assert pyautogui.scroll(-100) == 'down'
 
     # Send 'up' command and check if it scrolls up the page
     Arduino_Serial.write(b'up\n')
-    assert pyautogui.scroll(100) == b'up\n'
+    assert pyautogui.scroll(100) == 'up'
 
     # Send 'change' command and check if it performs 'alt+tab' operation
     Arduino_Serial.write(b'change\n')
-    assert pyautogui.keyDown('alt') == b'change\n'
-    assert pyautogui.press('tab') == b'change\n'
-    assert pyautogui.keyUp('alt') == b'change\n'
+    assert pyautogui.keyDown('alt') == 'change'
+    assert pyautogui.press('tab') == 'change'
+    assert pyautogui.keyUp('alt') == 'change'
