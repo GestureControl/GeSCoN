4c439ca2baf6302cb798e74ef29c4e07f3c2303f
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-04-18 17:04:16 +0530

    Update test_new1.py

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -1,27 +1,44 @@
+import serial
+import pyautogui
+from pyvirtualdisplay import Display
 import pytest
-import os
 
-@pytest.mark.xfail(reason="Flaky test due to GUI interaction")
-def test_serial():
-    os.environ['DISPLAY'] = ':0'
-    with XVFB():
-        import serial
-        import pyautogui
-        Arduino_Serial = serial.Serial('com12', 9600)
-        while 1:
-            incoming_data = str(Arduino_Serial.readline())
-            print(incoming_data)
-            if 'next' in incoming_data:
-                pyautogui.hotkey('ctrl', 'pgdn')
-            if 'previous' in incoming_data:
-                pyautogui.hotkey('ctrl', 'pgup')
-            if 'down' in incoming_data:
-                pyautogui.scroll(-100)
-            if 'up' in incoming_data:
-                pyautogui.scroll(100)
-            if 'change' in incoming_data:
-                pyautogui.keyDown('alt')
-                pyautogui.press('tab')
-                pyautogui.keyUp('alt')
-            incoming_data = ""
+# Initialize XVFB display
+display = Display(visible=0, size=(1024, 768))
+display.start()
+
+# Define the test function
+def test_serial_operations():
+    # Open a virtual serial port
+    Arduino_Serial = serial.Serial('com12',9600)
+    
+    # Loop for reading incoming data from the serial port
+    while True:
+        incoming_data = str(Arduino_Serial.readline())
+        print(incoming_data)
+        
+        if 'next' in incoming_data:
+            pyautogui.hotkey('ctrl', 'pgdn')
+            
+        if 'previous' in incoming_data:
+            pyautogui.hotkey('ctrl', 'pgup')
+            
+        if 'down' in incoming_data:
+            pyautogui.scroll(-100)
+            
+        if 'up' in incoming_data:
+            pyautogui.scroll(100)
+            
+        if 'change' in incoming_data:
+            pyautogui.keyDown('alt')
+            pyautogui.press('tab')
+            pyautogui.keyUp('alt')
+            
+        incoming_data = ""
+
+# Close the serial port and stop XVFB display after the test completes
+def teardown_function():
+    Arduino_Serial.close()
+    display.stop()
+
 
