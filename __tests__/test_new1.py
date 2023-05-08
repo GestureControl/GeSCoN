f01b6d0230b6111ac3b2d7cd275d4fa9e76dff93
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-05-08 23:57:31 +0530

    Update test_new1.py
    updated with testing time of flight equation to find distance between object and sensor and match both sensor computational output

--- __tests__/test_new1.py
+++ __tests__/test_new1.py
@@ -1,49 +1,22 @@
-import serial
-import pyautogui
-from pyvirtualdisplay import Display
-import pytest
-import os
-# Set DISPLAY environment variable
-os.environ['DISPLAY'] = ':0'
-# Define the test function
-def test_serial_operations():
-    
-    
-    # Start virtual display
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
+import math
+def distance1(duration):
+    r = 3.4 * duration / 2
+    dist1 = r / 100.00
+    return dist1
 
-    # Open a virtual serial port
-    Arduino_Serial = serial.Serial('com12',9600)
-    
-    # Loop for reading incoming data from the serial port
-    while True:
-        incoming_data = str(Arduino_Serial.readline())
-        print(incoming_data)
-        
-        if 'next' in incoming_data:
-            pyautogui.hotkey('ctrl', 'pgdn')
-            
-        if 'previous' in incoming_data:
-            pyautogui.hotkey('ctrl', 'pgup')
-            
-        if 'down' in incoming_data:
-            pyautogui.scroll(-100)
-            
-        if 'up' in incoming_data:
-            pyautogui.scroll(100)
-            
-        if 'change' in incoming_data:
-            pyautogui.keyDown('alt')
-            pyautogui.press('tab')
-            pyautogui.keyUp('alt')
-            
-        incoming_data = ""
+def distance2(duration):
+    r = 3.4 * duration / 2
+    dist2 = r / 100.00
+    return dist2
 
-    # Close the serial port and stop XVFB display after the test completes
-    Arduino_Serial.close()
-    display.stop()
-    
-# Define the teardown function for pytest
-def teardown_function():
-    pass
+duration = 1000.0
+
+Distance_1 = distance1(duration)
+def test_distance1():
+    assert int(Distance_1)==17    
+
+Distance_2 = distance2(duration)
+def test_distance2():
+    assert int(Distance_2)==17
+
+assert int(Distance_1) == int(Distance_2)
