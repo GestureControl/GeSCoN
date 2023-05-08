ffa0ae9bd5227b36b38809d8d6ce08a6bfd6a222
nizar_ahmed <95404661+Nizarahmed2002@users.noreply.github.com>
2023-05-09 01:08:40 +0530

    Update test_prototype.py

--- __tests__/test_prototype.py
+++ __tests__/test_prototype.py
@@ -1,18 +1,17 @@
 import math
-
 pointX1 = 10
 pointX2 = 7
 pointY1 = 16
 pointY2 = 12
-
-def get_dist(pointX1, pointX2, pointY1, pointY2):
-    sign = 1  # Default sign is positive
-    if pointY1 > pointY2:
+def get_dist(pointX1,pointX2,pointY1,pointY2):
+    sign = 1
+    if(pointY1<pointY2):
         sign = -1
-    dist = (pointX1 - pointX2) ** 2 + (pointY1 - pointY2) ** 2
+    dist = (pointX1 - pointX2)**2 
+    dist += (pointY1 - pointY2)**2
     dist = math.sqrt(dist)
-    return dist * sign
+    return dist*sign
 
+distance = get_dist(pointX1,pointX2,pointY1,pointY2)
 def test_get_dist():
-    distance = get_dist(pointX1, pointX2, pointY1, pointY2)
-    assert int(distance) == 5
+    assert int(distance)==5
